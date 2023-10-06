09e7bc97339210e80ae1d568d5e5ed3b9393d285
soleilsolis <fukuman23@gmail.com>
2023-10-07 06:26:37 +0800

    working

--- src/hooks/appointment.js
+++ src/hooks/appointment.js
@@ -20,63 +20,6 @@ export const useAppointment = () => {
             })
     }
 
-    const indexAppointments = () => {
-        const [appointments, getAppointments] = useState()
-        axios
-            .get('/api/appointments')
-            .then(res => {
-                getAppointments(
-                    <div className="grid lg:grid-cols-3 gap-2 md:gap-4 md:grid-cols-2 grid-cols-1">
-                        {res.data.data.map(appointment => (
-                            <Card className="transition ease-in-out mt-6 w-full ">
-                                <CardBody>
-                                    <Typography
-                                        variant="medium"
-                                        color="blue-gray">
-                                        #{appointment.id}
-                                    </Typography>
-                                    <Typography variant="h5" color="blue-gray">
-                                        {moment(
-                                            appointment.scheduled_at,
-                                        ).format('MMMM Do, YYYY')}
-                                    </Typography>
-                                    <Typography
-                                        variant="medium"
-                                        color="blue-gray">
-                                        Dr. Meredith Grey
-                                    </Typography>
-                                </CardBody>
-                                <CardFooter className="pt-0 inline-flex gap-2 flex-row-reverse md:flex-row">
-                                    <a
-                                        href={appointment.payment_link}
-                                        target="_blank"
-                                        rel="noreferrer">
-                                        <Button
-                                            variant="gradient"
-                                            className="rounded-full"
-                                            color="cyan">
-                                            Pay Now
-                                        </Button>
-                                    </a>
-                                    <Button
-                                        variant="text"
-                                        className="rounded-full"
-                                        color="cyan">
-                                        Details
-                                    </Button>
-                                </CardFooter>
-                            </Card>
-                        ))}
-                    </div>,
-                )
-            })
-            .catch(error => {
-                if (error.response.status !== 409) throw error
-            })
-
-        return appointments
-    }
-
     return {
         newAppointment,
         indexAppointments,
