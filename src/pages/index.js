4130230cf65fccd76c382ff86d1c5eaf8a956446
soleilsolis <fukuman23@gmail.com>
2023-10-07 09:42:19 +0800

    faste

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,38 +13,34 @@ export default function Home() {
             </Head>
 
             <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-                
-
                 <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
                     <div className="flex justify-center pt-8 sm:justify-start sm:pt-0">
                         <ApplicationLogo></ApplicationLogo>
                     </div>
 
                     <div className="py-4 text-center">
-                    {user ? (
-                        <Link
-                            href="/dashboard"
-                            className="text-xl text-cyan-700">
-                            Dashboard
-                        </Link>
-                    ) : (
-                        <>
+                        {user ? (
                             <Link
-                                href="/login"
+                                href="/dashboard"
                                 className="text-xl text-cyan-700">
-                                Login
+                                Dashboard
                             </Link>
+                        ) : (
+                            <>
+                                <Link
+                                    href="/login"
+                                    className="text-xl text-cyan-700">
+                                    Login
+                                </Link>
 
-                            <Link
-                                href="/register"
-                                className="ml-4 text-xl text-cyan-700">
-                                Register
-                            </Link>
-                        </>
-                    )}
-                </div>
-
-
+                                <Link
+                                    href="/register"
+                                    className="ml-4 text-xl text-cyan-700">
+                                    Register
+                                </Link>
+                            </>
+                        )}
+                    </div>
                 </div>
             </div>
         </>
