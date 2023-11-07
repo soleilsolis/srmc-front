4f5ae5d74d7fc75e58ffa09c2c349fb6a1d1ddbe
soleilsolis <fukuman23@gmail.com>
2023-11-07 17:58:56 +0800

    asdfbadsg24ergv

--- src/pages/register.js
+++ src/pages/register.js
@@ -194,6 +194,13 @@ const Register = () => {
                             onChange={event =>
                                 setContactNumber(event.target.value)
                             }
+                            onInput={event => {
+                                console.log(
+                                    event.target.value.match(
+                                        '/^(?!(?:0|0.0|0.00)$)[+]?d+(.d|.d[0-9])?$/',
+                                    ),
+                                )
+                            }}
                             required
                             autoComplete="contact_number"
                         />
