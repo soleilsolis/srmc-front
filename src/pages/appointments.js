9dd3b138ae065ca64186e4ac75ecfdc7af017e13
soleilsolis <fukuman23@gmail.com>
2023-11-16 02:05:33 +0800

    included Dialog

--- src/pages/appointments.js
+++ src/pages/appointments.js
@@ -20,6 +20,7 @@ import {
     Avatar,
     IconButton,
     Tooltip,
+    Dialog,
 } from '@material-tailwind/react'
 import { useEffect, useState, createElement } from 'react'
 import { useAppointment } from '@/hooks/appointment'
