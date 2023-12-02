786b1649f1cc61f8c824bddb7a1073e1bd62d808
soleilsolis <fukuman23@gmail.com>
2023-12-02 12:02:39 +0800

    vasdfdsf

--- src/pages/admin/supply/index.js
+++ src/pages/admin/supply/index.js
@@ -11,7 +11,6 @@ import {
     TabsHeader,
     Tabs,
     Tab,
-    CardFooter,
     Tooltip,
     IconButton,
 } from '@material-tailwind/react'
@@ -238,22 +237,6 @@ const Supply = () => {
                             </tbody>
                         </table>
                     </CardBody>
-                    <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
-                        <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal">
-                            Page 1 of 10
-                        </Typography>
-                        <div className="flex gap-2">
-                            <Button variant="outlined" size="sm">
-                                Previous
-                            </Button>
-                            <Button variant="outlined" size="sm">
-                                Next
-                            </Button>
-                        </div>
-                    </CardFooter>
                 </Card>
             </div>
         </AppLayout>
